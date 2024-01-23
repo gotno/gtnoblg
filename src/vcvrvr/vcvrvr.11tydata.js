c731798723f79f95b4621e2287c501707df9209a
Will Floyd <floyd.will@gmail.com>
2024-01-23 10:03:35 -0800

    hide some junk for a11y

--- src/vcvrvr/vcvrvr.11tydata.js
+++ src/vcvrvr/vcvrvr.11tydata.js
@@ -4,7 +4,7 @@ module.exports = {
   layout: "templates/_post_alone.liquid",
   tags: "vcvrvr_post",
   header_class: "vcvrvr",
-  header_markup: '<a href="/vcvrvr">vcvrvr</a>(vrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvr)',
+  header_markup: '<a href="/vcvrvr">vcvrvr</a><span aria-hidden="true">(vrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvr)</span>',
   eleventyComputed: {
     dateString: ({ page }) => DateTime.fromJSDate(page.date, { zone: 'utc' }).toLocaleString(DateTime.DATE_FULL),
   },
