520915e18f57391cb898b4f10d897c4d1a4e0be9
Will Floyd <floyd.will@gmail.com>
2024-01-26 09:18:49 -0800

    add eleventy-auto-cache-buster

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 const Image = require("@11ty/eleventy-img");
+const eleventyAutoCacheBuster = require("eleventy-auto-cache-buster");
 
 const imageShortcodeCallback =
   async function(src, alt = '', linkUrl = '') {
@@ -42,6 +43,8 @@ const ytShortcodeCallback =
 	};
 
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(eleventyAutoCacheBuster);
+
 	eleventyConfig.addShortcode('image', imageShortcodeCallback);
 	eleventyConfig.addShortcode('yt', ytShortcodeCallback);
 
